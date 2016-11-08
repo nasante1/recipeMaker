42fb02f03e2e86172b2c23ff848801bccd9b9999
Nathaniel Asante <nathanielasante@hotmail.co.uk>
2016-11-08 18:00:44 +0000

    Created form with post request

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  next()
+});
+
+router.use('/', (req,res)=> {
+  res.send(req.body.test)
 });
 
 module.exports = router;
